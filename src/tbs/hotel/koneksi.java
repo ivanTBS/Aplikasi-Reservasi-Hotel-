e3b431fcb30f68c6d5ec62da6b1e39b107be4423
ivanTBS <ivantbs11@gmail.com>
2018-03-26 22:37:02 +0700

    Add files via upload

---
+++ src/tbs/hotel/koneksi.java
@@ -0,0 +1,36 @@
+package tbs.hotel;
+
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author D2K
+ */
+public class koneksi {
+    private static Connection koneksi;
+   public static Connection getKoneksi(){
+    if(koneksi==null){
+      try{
+          String url = "jdbc:mysql://localhost:3306/rentalmobil";
+          String user = "root";
+          String pass = "";
+
+          DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+          koneksi= (Connection) DriverManager.getConnection(url,user,pass);
+      }
+      catch(SQLException t){
+         JOptionPane.showMessageDialog(null,"Error Membuat Koneksi");
+      }
+    }
+    return koneksi;
+  }
+}
